601e053d71b499cd0c15cba5d356692682ec498a
ZMRTZER26 <zmrtzer@byui.edu>
2024-09-26 16:57:53 -0600

    mission statement II

--- mission/mission.js
+++ mission/mission.js
@@ -1,3 +1,15 @@
-document.getElementsByTagName('select').addEventListener("change", function(){
+const themeSelect = document.getElementById("themeSelect");
+const body = document.body;
+const imageSelect = document.querySelector(".logo");
 
-})
+function changeTheme() {
+    if (themeSelect.value === "dark") {
+        body.classList.add("dark");
+        imageSelect.src = "byui-logo_white.png";
+    } else {
+        body.classList.remove("dark");
+        imageSelect.src = "byui-logo_blue.webp";
+    }
+};
+
+themeSelect.addEventListener('change', changeTheme);
